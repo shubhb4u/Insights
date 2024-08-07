3f22043959588081104fb5c56ca54477870739a9
Shubham Mazumdar <shubhammazumdar94@gmail.com>
2024-08-07 15:27:33 -0500

    related products

--- force-app/main/default/lwc/sldsCarouselCmsPanel/sldsCarouselCmsPanel.js
+++ force-app/main/default/lwc/sldsCarouselCmsPanel/sldsCarouselCmsPanel.js
@@ -1,36 +1,26 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { getContent } from 'experience/cmsDeliveryApi';
-import siteName from '@salesforce/schema/SiteName';
-import basePath from '@salesforce/community/basePath';
+import siteId from '@salesforce/site/Id';
 
 export default class SldsCarouselCmsPanel extends LightningElement {
   @api
-  contentKey = '20Yak000000QfM7EAK';
+  contentKey;
   @track data;
+  baseDomain= 'https://etgdigital6-dev-ed.develop.my.site.com/InsightsB2Bvforcesite/sfsites/c/';
   url;
 
-  @wire(getContent, { channelOrSiteId: siteName, contentKeyOrId: '$contentKey' })
+  @wire(getContent, { channelOrSiteId: siteId, contentKeyOrId: '$contentKey' })
   onGetContent(result) {
     if (result.data) {
       this.data = result.data;
-      console.log('data in child component ->>>>>>>>>>>>>> ', this.data);
-      console.log(
-        'data.unauthenticatedUrl -->>>>>>>> ',
-        result.data.unauthenticatedUrl
-      );
-      console.log('basePath is -->>>>> ', basePath);
-      this.formUrl();
+      console.log('url ----------------------- ', this.data);
+      console.log('this.data.contentBody.url ->>>>>>>>>>>>>> ', this.data.contentBody['sfdc_cms:media'].url);
+      this.url = this.baseDomain+ this.data.contentBody['sfdc_cms:media'].url;
+      
     } else if (result.error) {
       // Handle error, e.g., display a placeholder image or message
       console.error(result.error);
     }
   }
 
-  formUrl() {
-    const baseUrl = `https://${siteName}.develop.my.site.com`; // Dynamic base URL
-    const pathPrefix = '/sfsites/c'; // Adjust if your path structure is different
-
-    this.url = `${baseUrl}${basePath}${pathPrefix}${this.data.unauthenticatedUrl}`;
-    console.log('url is --->>>>>>> ', this.url);
-  }
 }
