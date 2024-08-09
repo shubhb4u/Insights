b69698c3373969ea073439cd58d9e82fe2a1ca39
Shubham Mazumdar <shubhammazumdar94@gmail.com>
2024-08-09 18:21:32 -0500

    In progress featured producvts

--- force-app/main/default/lwc/featuredProducts/featuredProducts.js
+++ force-app/main/default/lwc/featuredProducts/featuredProducts.js
@@ -1,14 +1,28 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import getProductRecs from '@salesforce/apex/FeaturedProductsController.getFeaturedProductsInsights';
+import getProductDiscountList from '@salesforce/apex/DisplayProductRecords.getProductRecsForDiscount';
 import { addItemToCart } from 'commerce/cartApi';
 
 
 export default class FeaturedProductsController extends LightningElement {
-    @track products = [];
+    @track products;
+    @track productDiscountList;
     @track displayedProducts = [];
     @track carouselIndicators = [];
     @track currentPage = 0;
     @track itemsPerPage = 4;
+    @track product2;
+
+    @wire(getProductDiscountList)
+    wiredProductDiscountList({ error, data }) {
+        if (data) {
+            this.productDiscountList = data;
+            console.log('Product discount list received:', this.productDiscountList);
+        } else if (error) {
+            console.error('Error retrieving product discount list:', error);
+        }
+    }
 
     connectedCallback() {
         this.fetchProducts();
@@ -17,34 +31,60 @@ export default class FeaturedProductsController extends LightningElement {
     fetchProducts() {
         getProductRecs()
             .then((data) => {
-                this.products = data.map(product => ({
-                    ...product,
-                    formattedUnitPrice: this.formatPrice(product.UnitPrice)
-                }));
+                this.products = data.map(product => {
+                    let discountedPrice = this.getFinalPrice(product);
+                    return {
+                        ...product,
+                        formattedUnitPrice: this.formatPrice(discountedPrice),
+                        isDiscounted: this.productDiscountList.some(discountProduct => discountProduct.Id === product.Product2.Id)
+                    };
+                });
                 this.setupCarousel();
-                console.log('Fetched products: --->>>>>> ', this.products);
+                console.log('this.products --->>>>>  ', this.products);
             })
             .catch((error) => {
                 console.error('Error fetching products:', error);
             });
     }
-    
-    formatPrice(price) {
-        // Ensure the price is a number, then format it to 2 decimal places
-        return Number(price).toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD',
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        });
+
+    getFinalPrice(product) {
+        this.showMSRP = false;
+        let finalPrice = product.UnitPrice;
+
+        // Ensure productDiscountList is defined and not empty
+        if (this.productDiscountList && this.productDiscountList.length > 0) {
+            const matchingDiscountProduct = this.productDiscountList.find(discountProduct => discountProduct.Id === product.Product2.Id);
+
+            if (matchingDiscountProduct) {
+                if (matchingDiscountProduct.isDiscounted__c) {
+                    this.showMSRP = true;
+                    finalPrice -= parseFloat((matchingDiscountProduct.AdjustmentPercent__c / 100) * finalPrice);
+                    console.log('Discount applied. Final price:', finalPrice);
+                } else if (matchingDiscountProduct.isDiscountedAmount__c) {
+                    this.showMSRP = true;
+                    finalPrice -= matchingDiscountProduct.Adjustment_Amount__c;
+                    console.log('Discount applied. Final price:', finalPrice);
+                }
+            }
+        } else {
+            console.warn('productDiscountList is undefined or empty. Returning original price.');
+        }
+
+        return finalPrice;
     }
-    
+
+    formatPrice(price) {
+        return price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }
+
     setupCarousel() {
         this.updateDisplayedProducts();
-        this.carouselIndicators = Array(Math.ceil(this.products.length / this.itemsPerPage)).fill().map((_, index) => ({
-            index,
-            class: `splide__pagination__page ${index === this.currentPage ? 'is-active' : ''}`
-        }));
+        this.carouselIndicators = Array(Math.ceil(this.products.length / this.itemsPerPage)).fill().map((_, index) => {
+            return {
+                index,
+                class: `splide__pagination__page ${index === this.currentPage ? 'is-active' : ''}`
+            };
+        });
     }
 
     updateDisplayedProducts() {
@@ -76,20 +116,35 @@ export default class FeaturedProductsController extends LightningElement {
     }
 
     updateIndicatorClasses() {
-        this.carouselIndicators = this.carouselIndicators.map((indicator) => ({
-            ...indicator,
-            class: `splide__pagination__page ${indicator.index === this.currentPage ? 'is-active' : ''}`
-        }));
+        this.carouselIndicators = this.carouselIndicators.map((indicator) => {
+            return {
+                ...indicator,
+                class: `splide__pagination__page ${indicator.index === this.currentPage ? 'is-active' : ''}`
+            };
+        });
     }
 
     handleBuy(event) {
         const productId = event.target.dataset.id;
-        let productName = event.target.dataset.name; // Use let instead of const
-        console.log('productId:---->>>>> ', productId);
-        console.log('productName: ----->>>>>>', productName);
+        let productName = event.target.dataset.name;
+
+        console.log('productId: --->>>> ', productId);
+        console.log('productName: ---->>>> ', productName);
 
         addItemToCart(productId, 1);
         location.reload();
-   
+    }
+
+    handleNavigate(event) {
+        const productId = event.target.dataset.id;
+        const productName = event.target.dataset.name;
+
+        let url = `https://etgdigital6-dev-ed.develop.my.site.com/InsightsB2B/product/${productName}/${productId}`;
+        this[NavigationMixin.Navigate]({
+            type: "standard__webPage",
+            attributes: {
+                url: url
+            },
+        });
     }
 }
